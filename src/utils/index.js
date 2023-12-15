f38901af6c8ceda39eee79b042bf11e5763616d4
rahul <rahulgotowork@gmail.com>
2023-12-15 19:35:36 +0530

    Project is full Ready

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,11 @@
-import FileSaver from "file-saver";
-
-const downloadFile = async (fileUrl, fileName) => {
-  // console.log("fileUrl: ", fileUrl);
-  // console.log("fileName: ", fileName);
-  const fileResponse = await fetch(fileUrl);
-  const file = await fileResponse.blob();
-  FileSaver.saveAs(file, fileName);
-};
+// import FileSaver from "file-saver";
+// const downloadFile = async (fileUrl, fileName) => {
+//   // console.log("fileUrl: ", fileUrl);
+//   // console.log("fileName: ", fileName);
+//   const fileResponse = await fetch(fileUrl);
+//   const file = await fileResponse.blob();
+//   FileSaver.saveAs(file, fileName);
+// };
 
 const pageTitle = (titleName) => {
   document.title = titleName;
